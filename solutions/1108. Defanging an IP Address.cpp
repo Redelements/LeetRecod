fa636eeffa8ae7e37d8a64b764cf7401c3c4fb17
Redelements <55516239+Redelements@users.noreply.github.com>
2020-07-21 12:50:38 -0700

    4 ms

---
+++ solutions/1108. Defanging an IP Address.cpp
@@ -0,0 +1,14 @@
+#include <string>
+using namespace std;
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        for(int i = 1;i < address.size(); i++){
+            if(address[i] == '.') {
+                address.replace(i,1,"[.]");
+                i++;
+            }
+        }
+     return address;
+    }  
+};
